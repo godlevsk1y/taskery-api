86d071d8f798438c3b51bb3fbe4e6c6afaadb9ec
Cyberbrain <godlevskiy.is.working@gmail.com>
2025-12-26 21:55:54 +0300

    feat(services): add ChangeEmail use case

--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -2,8 +2,10 @@ package services
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/cyberbrain-dev/taskery-api/internal/domain/user/models"
+	"github.com/cyberbrain-dev/taskery-api/internal/domain/user/vo"
 )
 
 // UserService is a service that handles user operations.
@@ -41,17 +43,35 @@ var ErrUserRepositoryNil = errors.New("user repository is nil")
 
 // Repository-level errors
 var (
-	// ErrUserRepoExists is returned by repository if the user already exists there
+	// ErrUserRepoExists is returned by repository if the user
+	// that is to be added to the repository already exists there
 	ErrUserRepoExists = errors.New("user already exists in the repository")
+
+	// ErrUserRepoNotFound is returned by repository if the user was not found there
+	ErrUserRepoNotFound = errors.New("user was not found in the repository")
 )
 
 // Application-level errors
 var (
-	// ErrUserExists is returned by UserService if the user already exists
+	// ErrUserExists is returned by UserService if the user that is to be added already exists
 	ErrUserExists = errors.New("user already exists")
+
+	// ErrUserNotFound is returned by UserService if the user was not found in the repository
+	ErrUserNotFound = errors.New("user was not found")
 
 	// ErrUserCreateFailed is returned by UserService if an error occured during creation
 	ErrUserCreateFailed = errors.New("failed to create user")
+
+	// ErrUserChangeEmailFailed is returned by UserService if an error occured during email editing
+	ErrUserChangeEmailFailed = errors.New("failed to change email")
+
+	// ErrUserUnauthorized is returned by UserService
+	// if the user does not have the necessary permissions to perform the operation.
+	ErrUserUnauthorized = errors.New("unauthorized access")
+
+	// ErrEmailAlreadyTaken is returned by UserService
+	// if the email that is to change the old one is already taken.
+	ErrEmailAlreadyTaken = errors.New("email is already taken")
 )
 
 // NewUserService creates a new instance of UserService with
@@ -82,6 +102,55 @@ func (us *UserService) Register(username, email, password string) error {
 		}
 
 		return ErrUserCreateFailed
+	}
+
+	return nil
+}
+
+// ChangeEmail updates the email address of an existing user.
+//
+// The method performs the following steps:
+//   - Retrieves the user by ID.
+//   - Verifies the provided password to ensure the user is authorized.
+//   - Checks that the new email is not already in use.
+//   - Updates the user's email and persists the change.
+//
+// Returns:
+//   - ErrUserNotFound if the user with the given ID does not exist.
+//   - ErrUserUnauthorized if the provided password is incorrect.
+//   - ErrEmailAlreadyTaken if the new email is already associated with another user.
+//   - ErrUserChangeEmailFailed for any internal or repository-related errors.
+func (us *UserService) ChangeEmail(id, newEmail, password string) error {
+	user, err := us.usersRepo.FindByID(id)
+	if errors.Is(err, ErrUserRepoNotFound) {
+		return ErrUserNotFound
+	}
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrUserChangeEmailFailed, err)
+	}
+
+	err = user.PasswordHash().Verify(password)
+	if errors.Is(err, vo.ErrPassowrdNotMatch) {
+		return ErrUserUnauthorized
+	}
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrUserChangeEmailFailed, err)
+	}
+
+	_, err = us.usersRepo.FindByEmail(newEmail)
+	if err == nil {
+		return ErrEmailAlreadyTaken
+	}
+	if !errors.Is(err, ErrUserNotFound) {
+		return fmt.Errorf("%w: %s", ErrUserChangeEmailFailed, err)
+	}
+
+	if err := user.ChangeEmail(newEmail); err != nil {
+		return err
+	}
+
+	if err := us.usersRepo.Update(user); err != nil {
+		return fmt.Errorf("%w: %s", ErrUserChangeEmailFailed, err)
 	}
 
 	return nil
