29724cf26cbaabce612bb861d58ea689d6f0b8ba
Cyberbrain <godlevskiy.is.working@gmail.com>
2026-01-13 14:39:11 +0300

    test(integration): add test for Create method of postgres user repository

---
+++ tests/integration/postgres/task_repository_test.go
@@ -0,0 +1,127 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	taskModels "github.com/cyberbrain-dev/taskery-api/internal/domain/task/models"
+	userModels "github.com/cyberbrain-dev/taskery-api/internal/domain/user/models"
+	"github.com/cyberbrain-dev/taskery-api/internal/services"
+
+	"github.com/cyberbrain-dev/taskery-api/internal/infrastructure/database/postgres"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func migrateTasks(t *testing.T, db *sql.DB) {
+	t.Helper()
+
+	_, err := db.Exec(`
+		CREATE TABLE users (
+			id UUID PRIMARY KEY,
+			username TEXT NOT NULL,
+			email TEXT NOT NULL UNIQUE,
+			password_hash TEXT NOT NULL
+		);
+	`)
+	require.NoError(t, err)
+
+	_, err = db.Exec(`
+		CREATE TABLE tasks (
+			id UUID PRIMARY KEY,
+			owner_id UUID NOT NULL,
+		
+			title TEXT NOT NULL,
+			description TEXT NOT NULL,
+		
+			deadline TIMESTAMPTZ NULL,
+		
+			is_completed BOOLEAN NOT NULL DEFAULT FALSE,
+			completed_at TIMESTAMPTZ NULL
+		);
+	`)
+
+	require.NoError(t, err)
+}
+
+func TestTaskRepository_Create(t *testing.T) {
+	db, cleanup := setupPostgres(t)
+	defer cleanup()
+
+	migrateTasks(t, db)
+
+	userRepo, err := postgres.NewUserRepository(db)
+	require.NoError(t, err)
+
+	taskRepo, err := postgres.NewTaskRepository(db)
+	require.NoError(t, err)
+
+	realUser, err := userModels.NewUserFromDB(userModels.UserFromDBParams{
+		ID:           uuid.New().String(),
+		Username:     "Test User",
+		Email:        "test@example.com",
+		PasswordHash: "password123",
+	})
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	err = userRepo.Create(ctx, realUser)
+	require.NoError(t, err)
+
+	validTask, err := taskModels.NewTask("title", "no description", realUser.ID())
+	require.NoError(t, err)
+
+	t.Run("success with nil deadline", func(t *testing.T) {
+		err = taskRepo.Create(ctx, validTask)
+		require.NoError(t, err)
+
+		var count int
+		err = db.QueryRow(`SELECT COUNT(*) FROM tasks`).Scan(&count)
+		require.NoError(t, err)
+
+		require.Equal(t, 1, count)
+	})
+
+	t.Run("success with deadline", func(t *testing.T) {
+		deadline := time.Now().Add(24 * time.Hour)
+		task, err := taskModels.NewTaskWithDeadline("other title", "some description", realUser.ID(), deadline)
+		require.NoError(t, err)
+
+		err = taskRepo.Create(ctx, task)
+		require.NoError(t, err)
+
+		var count int
+		err = db.QueryRow(`SELECT COUNT(*) FROM tasks`).Scan(&count)
+		require.NoError(t, err)
+
+		require.Equal(t, 2, count) // this db already contains user from prev test
+	})
+
+	t.Run("not existing owner", func(t *testing.T) {
+		task, err := taskModels.NewTask("not exist", "", uuid.New())
+		require.NoError(t, err)
+
+		err = taskRepo.Create(ctx, task)
+		require.ErrorIs(t, err, services.ErrTaskRepoOwnerNotFound)
+
+		var count int
+		err = db.QueryRow(`SELECT COUNT(*) FROM tasks`).Scan(&count)
+		require.NoError(t, err)
+
+		require.Equal(t, 2, count) // no more new tasks
+	})
+
+	t.Run("task already exists", func(t *testing.T) {
+		err = taskRepo.Create(ctx, validTask)
+		require.ErrorIs(t, err, services.ErrTaskRepoExists)
+
+		var count int
+		err = db.QueryRow(`SELECT COUNT(*) FROM tasks`).Scan(&count)
+		require.NoError(t, err)
+
+		require.Equal(t, 2, count) // no more new tasks
+	})
+}
