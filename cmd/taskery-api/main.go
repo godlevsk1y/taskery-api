749bf90a2ba07ce7e6657928563283a25997431c
Cyberbrain <godlevskiy.is.working@gmail.com>
2026-01-27 14:14:01 +0300

    feat(main): add setupLogger function

--- cmd/taskery-api/main.go
+++ cmd/taskery-api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/cyberbrain-dev/taskery-api/internal/infrastructure/config"
 )
@@ -10,4 +12,27 @@ func main() {
 	cfg := config.MustLoad()
 
 	fmt.Println(cfg)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case "local":
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+
+	case "dev":
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case "production":
+	default:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return logger
 }
