90b9d480cc780bda6441824171e200d3f4494adc
Cyberbrain <godlevskiy.is.working@gmail.com>
2026-01-31 18:04:07 +0300

    feat(http): rework WriteError so it could work with multiple errors

--- internal/infrastructure/transport/http/v1/handlers/error.go
+++ internal/infrastructure/transport/http/v1/handlers/error.go
@@ -3,15 +3,24 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
-type ErrorResponse struct {
-	Message string `json:"error"`
+type FieldError struct {
+	Field string `json:"field"`
+	Error string `json:"error"`
 }
 
-// WriteError writes given code and error to the HTTP response
+type ErrorResponse struct {
+	Error  string       `json:"error,omitempty"`
+	Errors []FieldError `json:"errors,omitempty"`
+}
+
+// WriteError writes given code and error to the HTTP response.
+// WriteError handles validation errors as well.
 func WriteError(w http.ResponseWriter, code int, err error) {
-	resp := ErrorResponse{Message: err.Error()}
+	resp := makeResponse(err)
 
 	data, mErr := json.Marshal(resp)
 	if mErr != nil {
@@ -22,4 +31,17 @@ func WriteError(w http.ResponseWriter, code int, err error) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	_, _ = w.Write(data)
+}
+
+func makeResponse(err error) ErrorResponse {
+	if vErrs, ok := err.(validator.ValidationErrors); ok {
+		fieldErrs := make([]FieldError, 0, len(vErrs))
+		for _, vErr := range vErrs {
+			fieldErrs = append(fieldErrs, FieldError{Field: vErr.Field(), Error: vErr.Error()})
+		}
+
+		return ErrorResponse{Errors: fieldErrs}
+	}
+
+	return ErrorResponse{Error: err.Error()}
 }
