afd0f1bec0a2069398a1490fda31759798794def
Cyberbrain <godlevskiy.is.working@gmail.com>
2026-01-31 19:58:28 +0300

    refactor(http): replace HandlerFunc factory with handler struct

--- internal/infrastructure/transport/http/v1/handlers/user/register.go
+++ internal/infrastructure/transport/http/v1/handlers/user/register.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"time"
 
 	"github.com/cyberbrain-dev/taskery-api/internal/infrastructure/transport/http/v1/handlers"
 	"github.com/cyberbrain-dev/taskery-api/internal/services"
@@ -20,50 +21,71 @@ type Registrar interface {
 	Register(ctx context.Context, username, email, password string) error
 }
 
-func Register(ctx context.Context, registrar Registrar, logger *slog.Logger, validate *validator.Validate) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		const op = "handlers.user.Register"
+type RegisterHandler struct {
+	registrar Registrar
+	timeout   time.Duration
+	logger    *slog.Logger
+	validate  *validator.Validate
+}
 
-		logger = logger.With(
-			slog.String("op", op),
-			slog.String("request_id", middleware.GetReqID(r.Context())),
-		)
-
-		var req RegisterRequest
-		err := json.NewDecoder(r.Body).Decode(&req)
-		if errors.Is(err, io.EOF) {
-			logger.Error("request body is empty")
-			handlers.WriteError(w, http.StatusBadRequest, errors.New("request body is empty"))
-			return
-		}
-		if err != nil {
-			logger.Error("failed to decode request body")
-			handlers.WriteError(w, http.StatusInternalServerError, errors.New("failed to decode request body"))
-			return
-		}
-
-		if err := validate.Struct(req); err != nil {
-			logger.Error("failed to validate request body", slog.String("error", err.Error()))
-			handlers.WriteError(w, http.StatusBadRequest, err)
-			return
-		}
-
-		err = registrar.Register(ctx, req.Username, req.Email, req.Password)
-		if err != nil {
-			logger.Error("failed to register user", slog.String("error", err.Error()))
-
-			if errorsx.IsAny(err, services.ErrUserExists) {
-				handlers.WriteError(w, http.StatusConflict, err)
-				return
-			}
-
-			handlers.WriteError(w, http.StatusInternalServerError, err)
-			return
-		}
-
-		handlers.WriteJSON(w, http.StatusCreated, RegisterResponse{
-			Username: req.Username,
-			Email:    req.Email,
-		})
+func NewRegisterHandler(
+	registrar Registrar,
+	timeout time.Duration,
+	logger *slog.Logger,
+	validate *validator.Validate) *RegisterHandler {
+	return &RegisterHandler{
+		registrar: registrar,
+		timeout:   timeout,
+		logger:    logger,
+		validate:  validate,
 	}
+}
+
+func (h *RegisterHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	const op = "handlers.user.Register"
+
+	ctx, cancel := context.WithTimeout(r.Context(), h.timeout)
+	defer cancel()
+
+	logger := h.logger.With(
+		slog.String("op", op),
+		slog.String("request_id", middleware.GetReqID(r.Context())),
+	)
+
+	var req RegisterRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if errors.Is(err, io.EOF) {
+		logger.Error("request body is empty")
+		handlers.WriteError(w, http.StatusBadRequest, errors.New("request body is empty"))
+		return
+	}
+	if err != nil {
+		logger.Error("failed to decode request body")
+		handlers.WriteError(w, http.StatusBadRequest, errors.New("failed to decode request body"))
+		return
+	}
+
+	if err := h.validate.Struct(req); err != nil {
+		logger.Error("failed to validate request body", slog.String("error", err.Error()))
+		handlers.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	err = h.registrar.Register(ctx, req.Username, req.Email, req.Password)
+	if err != nil {
+		logger.Error("failed to register user", slog.String("error", err.Error()))
+
+		if errorsx.IsAny(err, services.ErrUserExists) {
+			handlers.WriteError(w, http.StatusConflict, err)
+			return
+		}
+
+		handlers.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	handlers.WriteJSON(w, http.StatusCreated, RegisterResponse{
+		Username: req.Username,
+		Email:    req.Email,
+	})
 }
