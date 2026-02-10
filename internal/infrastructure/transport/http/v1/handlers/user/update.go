6428788627822f2a574b744b2ead01a7acf8dd22
godlevsk1y <godlevskiy.is.working@gmail.com>
2026-02-10 19:43:04 +0300

    refactor(http): change error msg when user is unauthorized in the UpdateHandler

--- internal/infrastructure/transport/http/v1/handlers/user/update.go
+++ internal/infrastructure/transport/http/v1/handlers/user/update.go
@@ -73,7 +73,7 @@ func (h *UpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			}
 
 			if errorsx.IsAny(err, services.ErrUserUnauthorized) {
-				handlers.WriteError(w, http.StatusUnauthorized, errors.New("password incorrect"))
+				handlers.WriteError(w, http.StatusUnauthorized, errors.New("unauthorized"))
 				return
 			}
 
