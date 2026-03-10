fb41c524fa1c9ececb403e417d3888d7e809b524
godlevsk1y <godlevskiy.is.working@gmail.com>
2026-03-10 11:55:55 +0300

    refactor(postgres): extract dsn composing to a separate function

--- internal/infrastructure/database/postgres/postgres.go
+++ internal/infrastructure/database/postgres/postgres.go
@@ -9,20 +9,24 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func DSN(cfg config.PostgresConnection) string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.Username,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DBName,
+		cfg.SSLMode,
+	)
+}
+
 // MustConnect opens a connection to Postgres database with provided configuration.
 // Panics if an error occurred
 func MustConnect(cfg config.PostgresConnection) *sql.DB {
 	const op = "postgres.Connect"
 
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host,
-		cfg.Port,
-		cfg.Username,
-		cfg.Password,
-		cfg.DBName,
-		cfg.SSLMode,
-	)
+	dsn := DSN(cfg)
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
